8a92b3ffd7111c075962b5dae1cfffe68d64f67b
Bradley Shawyer <bradley@blstr.co>
2020-06-20 18:33:27 +1000

    fix(map): install map-gl package

--- packages/website/components/Map.tsx
+++ packages/website/components/Map.tsx
@@ -1,5 +1,5 @@
 import { Component } from 'react'
-import ReactMapGL from 'react-map-gl'
+import ReactMapGL, { FlyToInterpolator } from 'react-map-gl'
 
 // @todo - Clear up this mess and useState hooks etc.
 class Map extends Component {
@@ -16,7 +16,16 @@ class Map extends Component {
     render() {
         return (
             <ReactMapGL
+                // @todo - Add Styles.
                 mapboxApiAccessToken="pk.eyJ1IjoidGhlY2hhbmdlYmUiLCJhIjoiY2tibmRybGdqMXJ5ajJ2cW43bnNub3A5dyJ9.BOp3bf8_eooK1IeohZbriA"
+                transitionDuration={1000}
+                transitionInterpolator={
+                    new FlyToInterpolator({
+                        curve: 1.414,
+                        speed: 1,
+                        maxDuration: 1000,
+                    })
+                }
                 onViewportChange={(viewport: any) =>
                     this.setState({ viewport })
                 }
