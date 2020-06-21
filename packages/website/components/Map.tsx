a69c42167dd57691c486884350984b9fa69066af
Bradley Shawyer <bradley@blstr.co>
2020-06-21 15:44:45 +1000

    fix(types): react-map-gl types working again

--- packages/website/components/Map.tsx
+++ packages/website/components/Map.tsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
-import { FlyToInterpolator, InteractiveMap } from 'react-map-gl'
+import { InteractiveMap } from 'react-map-gl'
 import { defaultMapSettings } from 'constants/map'
 
 export default ({ children }: { children: any }) => {
     const [viewport, setViewport] = useState(defaultMapSettings)
+    // const [activeSuburb, setActiveSuburb] = useState({ activeSuburb: '' })
 
     return (
         <InteractiveMap
@@ -11,15 +12,9 @@ export default ({ children }: { children: any }) => {
             mapStyle="mapbox://styles/thechangebe/ckbodarip393x1inx55ba34rg"
             mapboxApiAccessToken="pk.eyJ1IjoidGhlY2hhbmdlYmUiLCJhIjoiY2tibmRybGdqMXJ5ajJ2cW43bnNub3A5dyJ9.BOp3bf8_eooK1IeohZbriA"
             transitionDuration={1000}
-            transitionInterpolator={
-                new FlyToInterpolator({
-                    curve: 1.414,
-                    speed: 1,
-                    maxDuration: 1000,
-                })
-            }
             onViewportChange={(viewport: any) => setViewport({ ...viewport })}
             onClick={(e: any) => console.log(e)}
+            onMouseMove={(e: any) => console.log(e)}
             {...viewport}
         >
             {children}
